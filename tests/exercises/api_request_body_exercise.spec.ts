2baddc5d342d479f278e4f8648c4fa92a2727855
David Medek <d4vid.medek@gmail.com>
2025-12-03 17:12:59 +0100

    Api request GET, POST, asserts, reusing

---
+++ tests/exercises/api_request_body_exercise.spec.ts
@@ -0,0 +1,34 @@
+import { test } from "@playwright/test";
+// import { faker } from "@faker-js/faker";
+
+test("Exercise: POST Request", async ({ request }) => {
+  await request.post(
+    "https://tegb-backend-877a0b063d29.herokuapp.com/eshop/register",
+    {
+      data: {
+        username: "David.Medek",
+        password: "123456",
+        email: "David_Medek@example.net",
+      },
+    }
+  );
+});
+
+/*
+test("Exercise: POST Request with Body", async ({ request }) => {
+  const username =
+    faker.internet.username() + "_" + faker.number.int({ max: 1_000_000 });
+  const email = faker.internet.exampleEmail();
+
+  await request.post(
+    "https://tegb-backend-877a0b063d29.herokuapp.com/eshop/register",
+    {
+      data: {
+        username,
+        password: "123456",
+        email,
+      },
+    }
+  );
+});
+*/
---
+++ tests/learning/api/api_reusing_data.spec.ts
@@ -0,0 +1,36 @@
+import { expect, test } from "@playwright/test";
+import { faker } from "@faker-js/faker";
+
+test("Reusing Data Between API Calls", async ({ request }) => {
+  const email = faker.internet.email({ provider: "tredgate.cz" });
+  const password = "123456";
+  const username =
+    faker.internet.username() + "_" + faker.number.int({ max: 1_000_000 });
+  let userId = 0;
+  const registrationResponse = await request.post(
+    "https://tegb-backend-877a0b063d29.herokuapp.com/eshop/register",
+    {
+      data: {
+        username,
+        email,
+        password,
+      },
+    }
+  );
+  const registrationBody = await registrationResponse.json();
+  userId = registrationBody.userId;
+
+  const userResponse = await request.get(
+    "https://tegb-backend-877a0b063d29.herokuapp.com/eshop",
+    {
+      params: {
+        userId: userId,
+      },
+    }
+  );
+  const userBody = await userResponse.json();
+  expect(userBody.username, "userBody.username has Correct Value").toEqual(
+    username
+  );
+  expect(userBody.email, "userBody.email has Correct Value").toEqual(email);
+});
